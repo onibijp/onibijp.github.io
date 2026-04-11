d13a302bb30004132dca2ea4043eb0454ce6c87a
onibijp <phasemutation.prime@gmail.com>
2026-04-11 12:28:33 +0900

    Update maingamescript.js

--- maingamescript.js
+++ maingamescript.js
@@ -1,23 +1,18 @@
 const FPS = 60;
 const canvas = document.getElementById("gamecanvas");
 const draw = canvas.getContext("2d");
-const latest_date;
-const current_date;
 
 //----関数内で使う変数
-let draw_background_toggle = true;
+
 
 //----ループ処理
-init();
 function init(){
-latest_date = Date.now();
+  draw_background();
 
   
-  
-current_date = Date.now();  
-  setTimeout(() => init(), (1 / FPS) - (current_date - latest_date)); //1フレーム当たりの時間-このフレームの実行にかかった時間,待機すべき時間を計算
+window.requestAnimationFrame(init);
 }
-
+init();
 //----以下通常の処理
 function draw_background()
 {
